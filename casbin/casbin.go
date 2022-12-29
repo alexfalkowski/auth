ab7ae695186d8f6c7eea2304cb0b87d5e5a66c3a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-12-29 21:12:40 +0100

    refactor: move casbin

---
+++ casbin/casbin.go
@@ -0,0 +1,48 @@
+package casbin
+
+import (
+	"strings"
+
+	"github.com/casbin/casbin/v2/model"
+	"github.com/casbin/casbin/v2/persist"
+)
+
+// NewAdapter for a string policy.
+func NewAdapter(policy string) persist.Adapter {
+	return &adapter{policy: policy}
+}
+
+type adapter struct {
+	policy string
+}
+
+func (a *adapter) AddPolicy(sec, ptype string, rule []string) error {
+	return nil
+}
+
+func (a *adapter) LoadPolicy(model model.Model) error {
+	for _, p := range strings.Split(a.policy, "\n") {
+		p = strings.TrimSpace(p)
+		if p == "" {
+			continue
+		}
+
+		if err := persist.LoadPolicyLine(p, model); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *adapter) RemoveFilteredPolicy(sec, ptype string, fieldIndex int, fieldValues ...string) error {
+	return nil
+}
+
+func (a *adapter) RemovePolicy(sec, ptype string, rule []string) error {
+	return nil
+}
+
+func (a *adapter) SavePolicy(model model.Model) error {
+	return nil
+}
