74e60fc37d55f9ff14a8aedf28a2c481deefff81
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-12-30 20:31:06 +0100

    refactor(password): grpc error

--- server/v1/transport/grpc/server.go
+++ server/v1/transport/grpc/server.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"context"
 	"crypto/ed25519"
+	"errors"
 
 	v1 "github.com/alexfalkowski/auth/api/auth/v1"
 	"github.com/alexfalkowski/auth/key"
@@ -13,6 +14,8 @@ import (
 	"github.com/alexfalkowski/go-service/transport/grpc/meta"
 	"github.com/casbin/casbin/v2"
 	"go.uber.org/fx"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // ServerParams for gRPC.
@@ -53,12 +56,16 @@ type Server struct {
 func (s *Server) passwordAndHash(ctx context.Context, length uint32) (string, string, error) {
 	p, err := s.secure.Generate(ctx, password.Length(length))
 	if err != nil {
-		return "", "", err
+		if errors.Is(err, password.ErrInvalidLength) {
+			return "", "", status.Error(codes.InvalidArgument, err.Error())
+		}
+
+		return "", "", status.Error(codes.Internal, err.Error())
 	}
 
 	h, err := s.secure.Hash(ctx, p)
 	if err != nil {
-		return "", "", err
+		return "", "", status.Error(codes.Internal, err.Error())
 	}
 
 	return p, h, nil
