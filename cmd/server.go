a83abb9274985ab9ce0fdc196ef6987e81932605
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-09-22 16:32:11 +0200

    feat(deps): update github.com/alexfalkowski/go-service to v1.91.0 (#319)

--- cmd/server.go
+++ cmd/server.go
@@ -9,19 +9,18 @@ import (
 	v1 "github.com/alexfalkowski/auth/server/v1"
 	"github.com/alexfalkowski/auth/service"
 	"github.com/alexfalkowski/go-service/cache"
-	"github.com/alexfalkowski/go-service/logger"
 	"github.com/alexfalkowski/go-service/marshaller"
-	"github.com/alexfalkowski/go-service/metrics"
-	"github.com/alexfalkowski/go-service/otel"
 	"github.com/alexfalkowski/go-service/runtime"
+	"github.com/alexfalkowski/go-service/telemetry"
+	"github.com/alexfalkowski/go-service/telemetry/metrics"
 	"github.com/alexfalkowski/go-service/transport"
 	"go.uber.org/fx"
 )
 
 // ServerOptions for cmd.
 var ServerOptions = []fx.Option{
-	fx.NopLogger, runtime.Module, otel.Module, marshaller.Module,
+	fx.NopLogger, runtime.Module, telemetry.Module, metrics.Module, marshaller.Module,
 	cache.RistrettoModule, Module, config.Module, health.Module,
-	logger.ZapModule, metrics.PrometheusModule, transport.Module,
-	key.Module, casbin.Module, service.Module, password.Module, v1.Module,
+	transport.Module, key.Module, casbin.Module, service.Module,
+	password.Module, v1.Module,
 }
