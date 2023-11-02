a46176c15c83527d5123326d73b46cf8f558bedf
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-02 12:00:19 +0100

    fix(config): reduce lines

--- config/configurator.go
+++ config/configurator.go
@@ -14,11 +14,7 @@ import (
 func NewConfigurator(i *cmd.InputConfig) (config.Configurator, error) {
 	c := &Config{}
 
-	if err := i.Unmarshal(c); err != nil {
-		return nil, err
-	}
-
-	return c, nil
+	return c, i.Unmarshal(c)
 }
 
 func casbinConfig(cfg config.Configurator) *casbin.Config {
