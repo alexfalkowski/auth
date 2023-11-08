c60aaaf2d1fd55e333efab5df1b86ed87baeb699
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-08 20:12:24 +0100

    fix(client): remove admin token (#371)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -8,6 +8,5 @@ import (
 type Config struct {
 	Host    string        `yaml:"host" json:"host" toml:"host"`
 	Timeout time.Duration `yaml:"timeout" json:"timeout" toml:"timeout"`
-	Admin   string        `yaml:"admin" json:"admin" toml:"admin"`
 	Access  string        `yaml:"access" json:"access" toml:"access"`
 }
