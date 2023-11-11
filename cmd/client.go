dba4d639c0724068330cebe44f612300630e3efc
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-11 14:10:59 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.110.0

--- cmd/client.go
+++ cmd/client.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/alexfalkowski/auth/client"
 	"github.com/alexfalkowski/auth/config"
+	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
@@ -12,6 +13,7 @@ import (
 
 // ClientOptions for cmd.
 var ClientOptions = []fx.Option{
-	fx.NopLogger, runtime.Module, Module, telemetry.Module, metrics.Module,
+	fx.NopLogger, runtime.Module, Module, debug.Module,
+	telemetry.Module, metrics.Module,
 	config.Module, transport.Module, client.CommandModule,
 }
