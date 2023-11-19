21ab6879b773e87e3ae29c718c299e52b9d1d2bc
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-19 18:56:35 +0100

    fix(client): get from env (#398)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"time"
+
+	"github.com/alexfalkowski/go-service/os"
 )
 
 // Config for client.
@@ -9,4 +11,9 @@ type Config struct {
 	Host    string        `yaml:"host" json:"host" toml:"host"`
 	Timeout time.Duration `yaml:"timeout" json:"timeout" toml:"timeout"`
 	Access  string        `yaml:"access" json:"access" toml:"access"`
+}
+
+// GetAccess for client.
+func (c *Config) GetAccess() string {
+	return os.GetFromEnv(c.Access)
 }
