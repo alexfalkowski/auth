1db094e4fdb18dd05c7b06fa0e6822e08b481080
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-04 20:51:11 +0100

    feat(server): use client config (#495)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -4,13 +4,18 @@ import (
 	"time"
 
 	"github.com/alexfalkowski/go-service/os"
+	"github.com/alexfalkowski/go-service/retry"
+	"github.com/alexfalkowski/go-service/security"
 )
 
 // Config for client.
 type Config struct {
-	Host    string        `yaml:"host" json:"host" toml:"host"`
-	Timeout time.Duration `yaml:"timeout" json:"timeout" toml:"timeout"`
-	Access  string        `yaml:"access" json:"access" toml:"access"`
+	Host      string          `yaml:"host" json:"host" toml:"host"`
+	Security  security.Config `yaml:"security" json:"security" toml:"security"`
+	Timeout   time.Duration   `yaml:"timeout" json:"timeout" toml:"timeout"`
+	Retry     retry.Config    `yaml:"retry" json:"retry" toml:"retry"`
+	UserAgent string          `yaml:"user_agent" json:"user_agent" toml:"user_agent"`
+	Access    string          `yaml:"access" json:"access" toml:"access"`
 }
 
 // GetAccess for client.
