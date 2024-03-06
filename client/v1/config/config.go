5736de4ab15c3cb45797f88cd9d1a134d10d47a9
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-06 06:51:44 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.132.1 (#500)

--- client/v1/config/config.go
+++ client/v1/config/config.go
@@ -1,21 +1,14 @@
 package config
 
 import (
-	"time"
-
+	"github.com/alexfalkowski/go-service/config"
 	"github.com/alexfalkowski/go-service/os"
-	"github.com/alexfalkowski/go-service/retry"
-	"github.com/alexfalkowski/go-service/security"
 )
 
 // Config for client.
 type Config struct {
-	Host      string          `yaml:"host" json:"host" toml:"host"`
-	Security  security.Config `yaml:"security" json:"security" toml:"security"`
-	Timeout   time.Duration   `yaml:"timeout" json:"timeout" toml:"timeout"`
-	Retry     retry.Config    `yaml:"retry" json:"retry" toml:"retry"`
-	UserAgent string          `yaml:"user_agent" json:"user_agent" toml:"user_agent"`
-	Access    string          `yaml:"access" json:"access" toml:"access"`
+	Access        string `yaml:"access" json:"access" toml:"access"`
+	config.Client `yaml:",inline" json:",inline" toml:",inline"`
 }
 
 // GetAccess for client.
--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module github.com/alexfalkowski/auth
 
-go 1.22.0
+go 1.22.1
 
 require (
 	aidanwoods.dev/go-paseto v1.5.1
 	github.com/alexfalkowski/go-health v1.14.1
-	github.com/alexfalkowski/go-service v1.131.2
+	github.com/alexfalkowski/go-service v1.132.1
 	github.com/casbin/casbin/v2 v2.82.0
 	github.com/dgraph-io/ristretto v0.1.1
 	github.com/golang-jwt/jwt/v4 v4.5.0
@@ -18,7 +18,7 @@ require (
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.21.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20240125205218-1f4bbc51befe
-	google.golang.org/grpc v1.62.0
+	google.golang.org/grpc v1.62.1
 	google.golang.org/protobuf v1.32.0
 )
 
