62b4ec1f2e54ef18123e6987eaf5247b1e174e8a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-17 09:22:36 +0100

    feat(transport): use gRPC client

--- client/v1/transport/grpc/client.go
+++ client/v1/transport/grpc/client.go
@@ -5,8 +5,7 @@ import (
 
 	v1 "github.com/alexfalkowski/auth/api/auth/v1"
 	v1c "github.com/alexfalkowski/auth/client/v1/config"
-	"github.com/alexfalkowski/go-service/security/token"
-	"github.com/alexfalkowski/go-service/transport/grpc"
+	"github.com/alexfalkowski/auth/transport/grpc"
 	"github.com/alexfalkowski/go-service/transport/grpc/telemetry/tracer"
 	"go.opentelemetry.io/otel/metric"
 	"go.uber.org/fx"
@@ -19,7 +18,6 @@ type ServiceClientParams struct {
 
 	Lifecycle    fx.Lifecycle
 	ClientConfig *v1c.Config
-	TokenConfig  *token.Config
 	Logger       *zap.Logger
 	Tracer       tracer.Tracer
 	Meter        metric.Meter
@@ -27,41 +25,19 @@ type ServiceClientParams struct {
 
 // NewServiceClient for gRPC.
 func NewServiceClient(params ServiceClientParams) (v1.ServiceClient, error) {
-	if params.TokenConfig.Kind != "auth" {
-		return nil, nil
+	ctx := context.Background()
+	opts := grpc.ClientOpts{
+		Lifecycle:    params.Lifecycle,
+		ClientConfig: params.ClientConfig.Config,
+		Logger:       params.Logger,
+		Tracer:       params.Tracer,
+		Meter:        params.Meter,
 	}
 
-	opts := []grpc.ClientOption{
-		grpc.WithClientLogger(params.Logger), grpc.WithClientTracer(params.Tracer),
-		grpc.WithClientMetrics(params.Meter), grpc.WithClientRetry(params.ClientConfig.Retry),
-		grpc.WithClientUserAgent(params.ClientConfig.UserAgent),
-	}
-
-	sec := params.ClientConfig.Security
-	if sec != nil && sec.Enabled {
-		sec, err := grpc.WithClientSecure(params.ClientConfig.Security)
-		if err != nil {
-			return nil, err
-		}
-
-		opts = append(opts, sec)
-	}
-
-	conn, err := grpc.NewClient(context.Background(), params.ClientConfig.Host, opts...)
+	conn, err := grpc.NewClient(ctx, opts)
 	if err != nil {
 		return nil, err
 	}
-
-	params.Lifecycle.Append(fx.Hook{
-		OnStart: func(_ context.Context) error {
-			conn.ResetConnectBackoff()
-
-			return nil
-		},
-		OnStop: func(_ context.Context) error {
-			return conn.Close()
-		},
-	})
 
 	return v1.NewServiceClient(conn), nil
 }
---
+++ transport/grpc/grpc.go
@@ -0,0 +1,59 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/alexfalkowski/go-service/client"
+	"github.com/alexfalkowski/go-service/transport/grpc"
+	"github.com/alexfalkowski/go-service/transport/grpc/telemetry/tracer"
+	"go.opentelemetry.io/otel/metric"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	g "google.golang.org/grpc"
+)
+
+// ClientOpts for gRPC.
+type ClientOpts struct {
+	Lifecycle    fx.Lifecycle
+	ClientConfig client.Config
+	Logger       *zap.Logger
+	Tracer       tracer.Tracer
+	Meter        metric.Meter
+}
+
+// NewClient for gRPC.
+func NewClient(ctx context.Context, options ClientOpts) (*g.ClientConn, error) {
+	opts := []grpc.ClientOption{
+		grpc.WithClientLogger(options.Logger), grpc.WithClientTracer(options.Tracer),
+		grpc.WithClientMetrics(options.Meter), grpc.WithClientRetry(options.ClientConfig.Retry),
+		grpc.WithClientUserAgent(options.ClientConfig.UserAgent),
+	}
+
+	sec := options.ClientConfig.Security
+	if sec != nil && sec.Enabled {
+		sec, err := grpc.WithClientSecure(options.ClientConfig.Security)
+		if err != nil {
+			return nil, err
+		}
+
+		opts = append(opts, sec)
+	}
+
+	conn, err := grpc.NewClient(ctx, options.ClientConfig.Host, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	options.Lifecycle.Append(fx.Hook{
+		OnStart: func(_ context.Context) error {
+			conn.ResetConnectBackoff()
+
+			return nil
+		},
+		OnStop: func(_ context.Context) error {
+			return conn.Close()
+		},
+	})
+
+	return conn, nil
+}
