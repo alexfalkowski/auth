2cc3ad0dc50e7bbd97408a737070dfa0254a0438
Alex Falkowski <alexrfalkowski@gmail.com>
2024-04-01 10:41:15 +0200

    feat(deps): update github.com/alexfalkowski/go-service to v1.150.0

--- server/health/registrations.go
+++ server/health/registrations.go
@@ -1,12 +1,11 @@
 package health
 
 import (
-	"time"
-
 	h "github.com/alexfalkowski/auth/health"
 	"github.com/alexfalkowski/go-health/checker"
 	"github.com/alexfalkowski/go-health/server"
 	"github.com/alexfalkowski/go-service/health"
+	"github.com/alexfalkowski/go-service/time"
 	"go.uber.org/fx"
 )
 
@@ -18,15 +17,11 @@ type Params struct {
 }
 
 // NewRegistrations for health.
-func NewRegistrations(params Params) (health.Registrations, error) {
-	d, err := time.ParseDuration(params.Health.Duration)
-	if err != nil {
-		return nil, err
-	}
-
+func NewRegistrations(params Params) health.Registrations {
+	d := time.MustParseDuration(params.Health.Duration)
 	registrations := health.Registrations{
 		server.NewRegistration("noop", d, checker.NewNoopChecker()),
 	}
 
-	return registrations, nil
+	return registrations
 }
