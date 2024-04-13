a5360fc855209a30b6ba23778ea1c474300d0434
Alex Falkowski <alexrfalkowski@gmail.com>
2024-04-13 08:08:29 +0200

    feat(deps): github.com/alexfalkowski/go-service v1.154.0

--- server/v1/transport/grpc/grpc.go
+++ server/v1/transport/grpc/grpc.go
@@ -7,9 +7,9 @@ import (
 	v1c "github.com/alexfalkowski/auth/client/v1/config"
 	g "github.com/alexfalkowski/auth/transport/grpc"
 	"github.com/alexfalkowski/go-service/transport/grpc"
-	"github.com/alexfalkowski/go-service/transport/grpc/telemetry/tracer"
 	"github.com/alexfalkowski/go-service/transport/http"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -23,7 +23,7 @@ type RegisterParams struct {
 	HTTPServer   *http.Server
 	ClientConfig *v1c.Config
 	Logger       *zap.Logger
-	Tracer       tracer.Tracer
+	Tracer       trace.Tracer
 	Meter        metric.Meter
 	Server       v1.ServiceServer
 }
