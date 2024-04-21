fee795320553a2b2655230b3fe7a9f00d092b997
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-04-21 19:12:06 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.156.0 (#605)

--- cmd/client.go
+++ cmd/client.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"github.com/alexfalkowski/auth/client"
 	"github.com/alexfalkowski/auth/config"
+	"github.com/alexfalkowski/go-service/compressor"
 	"github.com/alexfalkowski/go-service/feature"
+	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
@@ -13,6 +15,7 @@ import (
 // ClientOptions for cmd.
 var ClientOptions = []fx.Option{
 	fx.NopLogger, runtime.Module, feature.Module,
+	compressor.Module, marshaller.Module,
 	telemetry.Module, metrics.Module,
 	config.Module, client.CommandModule, Module,
 }
--- cmd/rotate.go
+++ cmd/rotate.go
@@ -6,15 +6,18 @@ import (
 	"github.com/alexfalkowski/auth/password"
 	"github.com/alexfalkowski/auth/rotate"
 	"github.com/alexfalkowski/auth/token"
+	"github.com/alexfalkowski/go-service/compressor"
+	"github.com/alexfalkowski/go-service/feature"
+	"github.com/alexfalkowski/go-service/marshaller"
+	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"go.uber.org/fx"
 )
 
 // RotateOptions for cmd.
 var RotateOptions = []fx.Option{
-	fx.NopLogger,
-	telemetry.Module,
-	Module, config.Module,
-	key.Module, token.Module,
-	password.Module, rotate.CommandModule,
+	fx.NopLogger, runtime.Module, feature.Module,
+	telemetry.Module, compressor.Module, marshaller.Module,
+	config.Module, key.Module, token.Module,
+	password.Module, rotate.CommandModule, Module,
 }
