e9aabb9f3ee75cb8d0ab1368e85a62fe80122c51
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-09 18:42:46 +0200

    fix(client): move to module

--- client/module.go
+++ client/module.go
@@ -5,17 +5,10 @@ import (
 	"go.uber.org/fx"
 )
 
-var (
-	// Module for fx.
-	Module = fx.Options(
-		v1.Module,
-		fx.Provide(NewClient),
-		fx.Provide(NewToken),
-	)
-
-	// CommandModule for fx.
-	CommandModule = fx.Options(
-		Module,
-		fx.Invoke(RunCommand),
-	)
+// Module for fx.
+var Module = fx.Options(
+	v1.Module,
+	fx.Provide(NewClient),
+	fx.Provide(NewToken),
+	fx.Invoke(RunCommand),
 )
